7c11a46802780bad9846f502b421d7bf04c780b1
Denis <G00403126@atu.ie>
2023-12-12 15:48:02 +0000

    a

---
+++ src/test/java/ie/atu/microserviceb/MicroServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.microserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
