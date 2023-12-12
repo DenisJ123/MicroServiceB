de6f6051f0f7a8546a6068eb937546c251158332
Denis <G00403126@atu.ie>
2023-12-12 16:14:31 +0000

    Update

--- src/main/java/ie/atu/microserviceb/MicroServiceBApplication.java
+++ src/main/java/ie/atu/microserviceb/MicroServiceBApplication.java
@@ -2,7 +2,9 @@ package ie.atu.microserviceb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MicroServiceBApplication {
 
---
+++ src/main/java/ie/atu/microserviceb/RegisterController.java
@@ -0,0 +1,15 @@
+package ie.atu.microserviceb;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+    @PostMapping("/confirm")
+    public String confirmDetails(@RequestBody UserDetails userDetails) {
+        String confirmationMessage = String.format("Received details for %s with email %s", userDetails.getName(), userDetails.getEmail());
+            return confirmationMessage;
+
+    }
+}
